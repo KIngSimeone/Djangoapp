f4ac7261078cf943659ae138e973ac7bef207835
KIngSimeone <53562471+KIngSimeone@users.noreply.github.com>
2019-10-25 16:32:43 +0100

    Linked comment datbase to templates to view comments

--- myproject/guestbook/views.py
+++ myproject/guestbook/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Comment
+
 def index(request):
-    return render(request,'guestbook/index.html')
+    comments = Comment.objects.order_by('-date_added')
+
+    context = {'comments' : comments}
+
+    return render(request,'guestbook/index.html',context)
+
 def sign(request):
     return render(request,'guestbook/sign.html')
