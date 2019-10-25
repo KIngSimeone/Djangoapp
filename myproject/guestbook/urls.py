e3fac8a27f23b61b2e6e72f54db8e080d5a684cc
KIngSimeone <53562471+KIngSimeone@users.noreply.github.com>
2019-10-25 15:13:57 +0100

    Linked html templates

--- myproject/guestbook/urls.py
+++ myproject/guestbook/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('',views.index, name='index'),
-     path('sign/',views.sign, name='sign')
+     path('', views.index, name='index'),
+     path('sign/', views.sign, name='sign')
 ]
